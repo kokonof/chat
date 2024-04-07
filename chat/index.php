7f033dc0403ed4842fc061a967eb62bf6e366d7c
Admin <Admin@ADMIN>
2024-04-07 14:29:15 +0300

    logout.php

--- chat/index.php
+++ chat/index.php
@@ -21,8 +21,6 @@ if (!empty($_POST)) {
     }
 }
 $messages = getMessages();
-
-echo hash('sha256', $_SESSION['email']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -47,15 +45,39 @@ echo hash('sha256', $_SESSION['email']);
     </style>
 </head>
 <body>
+<nav class="py-2 bg-body-tertiary border-bottom">
+    <div class="container d-flex flex-wrap">
+        <ul class="nav me-auto">
+            <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2 active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Chat</a></li>
+        </ul>
+        <ul class="nav">
+
+            <?php
+            if(empty($_SESSION['email'])) {
+                echo '<li class="nav-item"><a  href="login.php" class="nav-link link-body-emphasis px-2">Login</a></li>';
+                echo '<li class="nav-item"><a  href="register.php" class="nav-link link-body-emphasis px-2">Register</a></li>';
+            } else {
+
+                echo '<li class="nav-item"><a  href="logout.php" class="nav-link link-body-emphasis px-2">Logout</a></li>';
+            }
+            ?>
 
 
-<!-- Хедер з посиланнями на головну і чат -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="#">Головна</a>
-        <a class="navbar-brand" href="#">Чат</a>
+        </ul>
     </div>
 </nav>
+<header class="py-3 mb-4 border-bottom">
+    <div class="container d-flex flex-wrap justify-content-center">
+        <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+            <span class="fs-4">Double header</span>
+        </a>
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
+            <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+        </form>
+    </div>
+</header>
 
 <!-- Контейнер для центрування контенту -->
 <div class="container justify-content-center align-items-center mt-4" style="min-height: 80vh;">
@@ -65,7 +87,8 @@ echo hash('sha256', $_SESSION['email']);
             <form method="post">
                 <div class="mb-3">
                     <label for="username" class="form-label">Your Name</label>
-                    <input type="text" class="form-control" id="username" name="username" value="<?= !empty($_SESSION['email']) ? $_SESSION['email']: ''?>">
+                    <input type="text" class="form-control" id="username" name="username"
+                           value="<?= !empty($_SESSION['email']) ? $_SESSION['email']: ''?>">
                 </div>
                 <div class="mb-3">
                     <label for="message" class="form-label">header</label>
