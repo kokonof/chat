47e6e3998f5e7d80c5dbf24577878ea379f3babd
Гріша Волков <Volkovgrisha80@gmail.com>
2024-05-30 11:10:47 +0300

    переорбив головну сторінку

--- chat/index.php
+++ chat/index.php
@@ -1,2 +1,36 @@
-<li class="nav-item"><a href="index.php" class="nav-link link-body-emphasis px-2 active" aria-current="page">Головна</a></li>
-<li class="nav-item"><a href="chat.php" class="nav-link link-body-emphasis px-2">Чат</a></li>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Головна сторінка</title>
+    <link rel="stylesheet" href="Styles.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li class="left-item"><a href="special.php">Спеціальна Сторінка</a></li>
+            <li class="nav-item"><a href="index.php" class="nav-link link-body-emphasis px-2 active" aria-current="page">Головна</a></li>
+            <li class="nav-item"><a href="#">Про нас</a></li>
+            <li class="nav-item"><a href="chat.php" class="nav-link link-body-emphasis px-2">Чат</a></li>
+            <li class="nav-item"><a href="#">Послуги</a></li>
+            <li class="nav-item"><a href="#">Контакти</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <section class="hero">
+        <h1>Ласкаво просимо на наш сайт</h1>
+        <p>Тут нічого немає, приходьте через рік.</p>
+    </section>
+    <section class="content">
+        <h2>Наші послуги</h2>
+        <p>"Розпис піздюлєй"</p>
+    </section>
+</main>
+<footer>
+    <p>&copy; 2024 Ваша Компанія</p>
+</footer>
+</body>
+</html>
